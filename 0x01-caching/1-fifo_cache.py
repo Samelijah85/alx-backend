cd494005c1ec4e1e8cae070fbf2a395bce737f81
EvanieWares <evaniewares@gmail.com>
2024-07-24 17:30:03 +0200

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module 1-fifo_cache
+
+Contains a class FIFOCache that inherits from BaseCaching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    A class that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        super().__init__()
+        self.key_indexes = []
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_discarded = self.key_indexes.pop(0)
+                del self.cache_data[item_discarded]
+                print("DISCARD:", item_discarded)
+
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
